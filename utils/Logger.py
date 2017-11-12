108c77eda50cab85b7250031fdd33c4e68160888
BrandenB <18152900+BrandenB@users.noreply.github.com>
2017-11-11 20:03:04 -0400

    - Added missing logger functions

--- utils/Logger.py
+++ utils/Logger.py
@@ -1,4 +1,16 @@
 import datetime
 
+debug = False
+
 def writeLine(message):
 	print('[' + datetime.datetime.now().strftime('%Y-%m-%d @ %I:%M:%S.%f') + ']', message)
+
+def errorLine(message):
+	print('[' + datetime.datetime.now().strftime('%Y-%m-%d @ %I:%M:%S.%f') + '] [ERROR]', message)
+
+def debugLine(message):
+	if (debug):
+		print('[' + datetime.datetime.now().strftime('%Y-%m-%d @ %I:%M:%S.%f') + '] [DEBUG]', message)
+
+def setDebug(toggle):
+	debug = toggle
